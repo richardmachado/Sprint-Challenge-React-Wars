d91e932ff5e9a4338c7e909830f66c2a8d3979f5
Lambda_School_Loaner_198 <lambda_school_loaner_198@Lambda-School-Loaner-198s-MacBook-Pro.local>
2019-09-27 20:01:36 -0700

    should be all done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import CharacterList from './components/CharacterList';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      starwarsChars: [],
+      next: '',
+      previous: ''
+    };
+  }
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  componentDidMount() {
+    this.getCharacters('https://swapi.co/api/people');
+  }
 
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
+  getCharacters = URL => {
+    // feel free to research what this code is doing.
+    // At a high level we are calling an API to fetch some starwars data from the open web.
+    // We then take that data and resolve it our state.
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data)
+        this.setState({ 
+          starwarsChars: data.results,
+          next: data.next,
+          previous: data.previous
+         });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+
+  nextHandler = (event) => {
+    event.preventDefault();
+    this.getCharacters(this.state.next)
+  }
+
+  previousHandler = (event) => {
+    event.preventDefault();
+    this.getCharacters(this.state.previous)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="Header">React Wars</h1>
+        <CharacterList value={this.state.starwarsChars} />
+        <button className={this.state.previous ? null : "hide" }  onClick={this.previousHandler}>Previous</button>
+        <button className={this.state.next ? null : "hide" } onClick={this.nextHandler}>Next</button>
+      </div>
+    );
+  }
 }
 
 export default App;
